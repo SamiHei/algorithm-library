fe75019f8653a7ce3e00fc3efc9c217cb600f942
sami_heikkinen <zami.heikkinen@hotmail.com>
2019-08-08 18:24:27 +0300

    Refactored add_node_in_order function

--- python/binarytree/binary_tree.py
+++ python/binarytree/binary_tree.py
@@ -8,36 +8,23 @@ class BinaryTree:
     def __init__(self, root):
         self.root = root
 
-    def add_node_in_order(self, key):
+    def add_node_in_order(self, root, key):
         """
         Adds new node with given value (key) to the correct spot in,
         binary tree (as in value order)
 
         Args:
+        root (Node): Root node of the binary tree
         key (int): Value for new node to be added
         """
-        if(self.root.left and self.root.key >= key):
-            latest_node = self.root.left
-            self._helper_add_node(latest_node, Node(key))
-        elif(self.root.right and self.root.key < key):
-            latest_node = self.root.right
-            self._helper_add_node(latest_node, Node(key))
-        elif(self.root.left is None and self.root.key >= key):
-            self.root.left = Node(key)
-        elif(self.root.right is None and self.root.key < key):
-            self.root.right = Node(key)
-
-    def _helper_add_node(self, latest_node, node):
-        if(latest_node.left is None and latest_node.key >= node.key):
-            latest_node.left = node
-            return
-        elif(latest_node.right is None and latest_node.key < node.key):
-            latest_node.right = node
-            return
-        elif(latest_node.left and latest_node.key >= node.key):
-            self._helper_add_node(latest_node.left, node)
-        elif(latest_node.right and latest_node.key < node.key):
-            self._helper_add_node(latest_node.right, node)
+        if(root.left and root.key >= key):
+            self.add_node_in_order(root.left, key)
+        elif(root.right and root.key < key):
+            self.add_node_in_order(root.right, key)
+        elif(root.left is None and root.key >= key):
+            root.left = Node(key)
+        elif(root.right is None and root.key < key):
+            root.right = Node(key)
 
     def traverse_tree_pre_order(self, root):
         """
@@ -52,8 +39,8 @@ class BinaryTree:
         node_keys = []
         if root:
             node_keys.append(root.key)
-            node_keys = node_keys + self.print_tree_pre_order(root.left)
-            node_keys = node_keys + self.print_tree_pre_order(root.right)
+            node_keys = node_keys + self.traverse_tree_pre_order(root.left)
+            node_keys = node_keys + self.traverse_tree_pre_order(root.right)
         return node_keys
 
     def traverse_tree_in_order(self, root):
@@ -68,9 +55,9 @@ class BinaryTree:
         """
         node_keys = []
         if root:
-            node_keys = node_keys + self.print_tree_in_order(root.left)
+            node_keys = node_keys + self.traverse_tree_in_order(root.left)
             node_keys.append(root.key)
-            node_keys = node_keys + self.print_tree_in_order(root.right)
+            node_keys = node_keys + self.traverse_tree_in_order(root.right)
         return node_keys
 
     def traverse_tree_post_order(self, root):
@@ -85,7 +72,7 @@ class BinaryTree:
         """
         node_keys = []
         if root:
-            node_keys = node_keys + self.print_tree_post_order(root.left)
-            node_keys = node_keys + self.print_tree_post_order(root.right)
+            node_keys = node_keys + self.traverse_tree_post_order(root.left)
+            node_keys = node_keys + self.traverse_tree_post_order(root.right)
             node_keys.append(root.key)
         return node_keys
