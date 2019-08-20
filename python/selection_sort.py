24add006182f5ae0744858849013a59f005ce9fd
Sami Heikkinen <zami.heikkinen@hotmail.com>
2019-08-20 13:27:25 +0300

    Added selection sort algorithm

---
+++ python/selection_sort.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+
+import random
+
+def main():
+    # Test function here
+    test_array = []
+    for i in range(50):
+        test_array.append(random.randrange(0, 10000))
+    print(test_array)
+    print("==========================")
+    print(selection_sort(test_array))
+
+
+def selection_sort(array):
+    array_length = len(array)
+    for i in range(array_length):
+        smallest_value = array[i]
+        smallest_value_index = i
+        
+        for j in range(i+1, array_length):
+            if(array[j] < smallest_value):
+                smallest_value = array[j]
+                smallest_value_index = array.index(smallest_value)
+               
+        if(array[smallest_value_index] < array[i]):
+           array[i], array[smallest_value_index] = array[smallest_value_index], array[i]
+
+    return array
+
+
+if __name__ == "__main__":
+    main()
