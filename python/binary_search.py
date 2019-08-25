ebc7f61512cb407070e0e1fa9f8c0d1e89c152ee
sami_heikkinen <zami.heikkinen@hotmail.com>
2019-08-25 11:05:25 +0300

    Optimized binary search algorithm

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,11 +1,14 @@
 #! /usr/bin/python3
 
-import random
 
 def main():
     # Test function here
-    test_array = [155, 220, 707, 997, 1013, 1125, 1300, 1346, 1591, 1716, 1864, 1873, 1923, 2044, 2432, 2677, 2994, 3085, 4030, 4058, 4172, 4378, 4424, 4547, 4598, 4791, 4797, 5961, 6044, 6488, 6640, 6703, 6867, 6873, 6989, 7671, 7823, 7850, 7909, 8155, 8248, 8437, 8662, 9030, 9454, 9553, 9661, 9760, 9789, 9899]
-    print(binary_search(test_array, 6703))
+    test_array = [155, 220, 707, 997, 1013, 1125, 1300, 1346, 1591, 1716, 1864,
+                  1873, 1923, 2044, 2432, 2677, 2994, 3085, 4030, 4058, 4172,
+                  4378, 4424, 4547, 4598, 4791, 4797, 5961, 6044, 6488, 6640,
+                  6703, 6867, 6873, 6989, 7671, 7823, 7850, 7909, 8155, 8248,
+                  8437, 8662, 9030, 9454, 9553, 9661, 9760, 9789, 9899]
+    print(binary_search(test_array, 4791))
 
 
 def binary_search(array, search_value):
@@ -22,20 +25,19 @@ def binary_search(array, search_value):
     index = 0
     while(array_length > 1):
         if(search_value < array[array_length//2] and array_length > 1):
-            index += binary_search(array[:array_length//2], search_value)
+            index += binary_search(array[:(array_length//2)], search_value)
         elif(search_value > array[array_length//2] and array_length > 1):
-            index = binary_search(array[array_length//2:], search_value)
-            index += array_length//2
+            index = binary_search(array[((array_length//2)+1):], search_value)
+            index += ((array_length//2)+1)
         elif(search_value == array[array_length//2]):
             index += array_length//2
             return index
         break
 
-    if(search_value != array[index]):
+    if(index > (array_length-1) or search_value != array[index]):
         return -1
     return index
 
 
 if __name__ == "__main__":
     main()
-
